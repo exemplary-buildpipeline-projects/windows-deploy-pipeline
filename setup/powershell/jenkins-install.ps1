336eab8f5c0c7df7df2a76131434e383ef7cb8aa
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-11-25 23:29:29 +0900

    シェイプしたスクリプトへ変更。

--- setup/powershell/jenkins-install.ps1
+++ setup/powershell/jenkins-install.ps1
@@ -1,23 +1,33 @@
-# Windows上にJenkinsの最新をインストールするスクリプト
+# Windows上にJenkinsの最新をインストールするPowerShellスクリプト
 #
-# 前提として「PowerShell-V5」でしか動かない。
-# (下位互換を極力考えたが、機能がしょぼすぎて記述がつらすぎる)
+# PowerShellV4を期待(Expand-Archive なんてもっと前からつけとけよ！)
+# パッケージ管理 chocolatey 経由でインストールする。(自力を断念)
+# ※おそらく、2015/12 付近でしか動かない。(Pluginの名前などがこの時点基準であるため)
 
-#　定数宣言
-$WORKDIR = ".\work"
-$JENKINS_INST_URL = "http://mirrors.jenkins-ci.org/windows/latest"
+# 定数
+$JENKINS_URL = "http://localhost:8080"
+$JENKINS_HOME = "C:\Program Files\Jenkins"
+$DEFAULT_ZIP = ".\default_setting_and_jobs.zip"
 
-# 作業用ディレクトリ作成 & カレント移動
-if (Test-Path $WORKDIR) { Remove-Item $WORKDIR -Recurse; }
-mkdir $WORKDIR
-cd $WORKDIR
+# 実処理
 
-# Jenkinsインストーラダウンロード
-Invoke-WebRequest -Uri $JENKINS_INST_URL -OutFile .\jenkins.zip
-# 解凍
-Expand-Archive .\jenkins.zip -DestinationPath ./
-# サイレントインストール
-# msiexec.exe /i .\jenkins.msi /quiet /l* ./jekins-install.log
-#.\Setup.exe /v"/qn"
-msiexec /i jenkins.msi /quiet JENKINSDIR="C:\Program Files\Jenkins\"
-# TODO が、上記がうまく動かない。
+# chocolatey 自体をインストール
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Jenkinsインストール
+choco install jre8
+choco install jenkins
+
+# JekinsCLIを使って、必要と思われるPluginを入れる。
+
+# Jenkins自体からjenkins-cli.jarを吐き出させる(PowerShellのそうとう古いやつでもいけるwget構文)
+$client = new-object System.Net.WebClient
+$client.DownloadFile( "$JENKINS_URL/jnlpJars/jenkins-cli.jar", "$pwd\jenkins-cli.jar" )
+
+# Pluginインストール
+# TODO java -jar jenkins-cli.jar -s $JENKINS_URL install-plugins pluginNameA pluginNameB pluginNameC...
+# FIXME ここはあとで再起動するからいらない…かも？ java -jar jenkins-cli.jar -s $JENKINS_URL safe-restart
+
+# 本体の設定 & ジョブの設定をZIPから展開し、インストールディレクトリにかぶせる & 再起動
+Expand-Archive -Path $DEFAULT_ZIP -DestinationPath $JENKINS_HOME -Force
+java -jar jenkins-cli.jar -s $JENKINS_URL safe-restart
