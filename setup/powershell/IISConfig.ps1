cf58f86f5a7c172c87a0e7050e2ac6b821879126
kazuhito-m <kazuhito.sumpic@gmail.com>
2015-12-02 21:09:17 +0900

    IISにマネジメントコンソールを追加するスクリプトを追加。

--- setup/powershell/IISConfig.ps1
+++ setup/powershell/IISConfig.ps1
@@ -19,6 +19,31 @@ Configuration IISConfiguration
            Ensure = "Present"
            Name = "Web-Asp-Net45"
        }
+       WindowsFeature MNG_CONSOLE
+       {
+           Ensure = "Present"
+           Name = "Web-Mgmt-Compat"
+       }
+       WindowsFeature MNG_CONSOLE2
+       {
+           Ensure = "Present"
+           Name = "Web-WMI"
+       }
+       WindowsFeature MNG_CONSOLE3
+       {
+           Ensure = "Present"
+           Name = "Web-Lgcy-Mgmt-Console"
+       }
+       WindowsFeature MNG_CONSOLE4
+       {
+           Ensure = "Present"
+           Name = "Web-Mgmt-Service"
+       }
+       WindowsFeature MNG_CONSOLE5
+       {
+           Ensure = "Present"
+           Name = "Web-Mgmt-Console"
+       }
    }
 }
 
